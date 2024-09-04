ab74adfb935cf8f1b552270a6aeead9f8554b3b8
Anneloesvb <anneloesvanbladel@gmail.com>
2024-09-04 14:18:36 +0200

    gebruikers knop toegevoegd met functie naarGebruiker

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 werkdag applicatie <br>
+
+<script>
+    function naarGebruiker(p){
+        window.open("werkdag_overzicht.php?gebid="+p);
+    }
+    </script>
+
 <?php
 require 'functies_beschikbaarheidsprikker.php';
 
 $mysqli = verbinding_maken_met_database();
 $result = selecteer_alles_van_tabel("gebruiker",$mysqli);
 while($row = $result -> fetch_assoc()){
-    print($row["voornaam"]."<br>");
+    print($row["voornaam"]);
+    echo '<button onclick="naarGebruiker('.$row["id"].')" class="naarGebruiker">Naar gebruiker</button>'; 
+    echo '<br>';
 }
 ?>
 
