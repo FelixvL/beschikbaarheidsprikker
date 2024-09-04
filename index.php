ad062b8825507d02179d6569841d31c38a83dea3
felixvl <digitaleservice@gmail.com>
2024-09-04 14:42:15 +0200

    even

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 werkdag applicatie <br>
 <?php
+
 require 'functies_beschikbaarheidsprikker.php';
 
-$mysqli = verbinding_maken_met_database();
-$result = selecteer_alles_van_tabel("gebruiker",$mysqli);
+$db_verbinding = verbinding_maken_met_database();
+
+$result = selecteer_alles_van_tabel("gebruiker", $db_verbinding);
+
 while($row = $result -> fetch_assoc()){
     print($row["voornaam"]."<br>");
 }
+
 ?>
 
 
